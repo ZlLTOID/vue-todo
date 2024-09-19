496984490ada8c776367bceacdea3d1fa8fa94ec
JanDolezal <dolezal6@gmail.com>
2024-09-19 16:13:51 +0200

    Remove HelloWorld component and integrate Bootstrap table
    Replaced the HelloWorld component with a Bootstrap-based employee management table. Modified index.html and global styles to use Bootstrap and related libraries. Added new components for editing and deleting employee records within modals.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeesTable from './components/EmployeesTable.vue';
+import EditEmployeeFormModal from './components/forms/EditEmployeeFormModal.vue';
+import DeleteEmployeeFormModal from './components/forms/DeleteEmployeeFormModal.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <EmployeesTable>
+      <h2>Manage <b>Employees</b></h2>
+    </EmployeesTable>
+	<EditEmployeeFormModal />
+	<DeleteEmployeeFormModal />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
