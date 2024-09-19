61b870b003123b219c1d64fbf6be2846b439681f
JanDolezal <dolezal6@gmail.com>
2024-09-19 16:17:16 +0200

    Add UI components for employee management
    Added Button.vue for reusable UI buttons with customizable properties. Implemented DeleteEmployeeFormModal.vue for employee deletion confirmation and EditEmployeeFormModal.vue for editing employee details within a modal dialog. These components enhance the user interface for managing employee records effectively.

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,24 @@
+<script setup>
+    defineProps({
+        class: {
+            type: String,
+            default: 'btn btn-success'
+        },
+        href: {
+            type: String,
+            default: '#addEmployeeModal'
+        },
+        text: {
+            type: String,
+            default: 'Add New Employee'
+        },
+        icon: {
+            type: String,
+            default: '&#xE147;'
+        },
+    });
+</script>
+
+<template>
+    <a :href="href" :class="class" data-toggle="modal"><i class="material-icons" v-html="icon"></i><span>{{ text }}</span></a>
+</template>
